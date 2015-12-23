e3d2c3fdc55a321bfa165aa0c388ad155b0ce87e
avi <hi@avi.im>
2015-12-23 22:22:23 +0530

    Add settings file

---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+
+TELEGRAM_ACCESS_TOKEN = os.environ['TELEGRAM_ACCESS_TOKEN']
+GOODREADS_API_KEY = os.environ['GOODREADS_API_KEY']
+
+# Telegram Webhook settings
+WEBHOOK_URL = os.environ['WEBHOOK_URL']
