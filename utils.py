8545528c07ed9eaf006a49cf1b875dcec70e89c7
avi <hi@avi.im>
2015-12-24 09:44:35 +0530

    Use `strip_html_tags`

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import re
 import textwrap
 
 import html2text
@@ -6,17 +7,22 @@ text_maker = html2text.HTML2Text()
 text_maker.body_width = 0
 
 
-def html_to_md(string):
-    string = string.replace('<br>', ' ')
+def strip_html_tags(text):
+    return re.sub('<[^<]+?>', '', text)
+
+
+def html_to_md(string, strip_html=True):
+    if strip_html is True:
+        string = strip_html_tags(string)
     return text_maker.handle(string)
 
 
 def get_formatted_book_data(book_data):
     template = textwrap.dedent("""\
-        **Title:** {0} by {1} 
-        **Rating:** {2} by {3} users 
-        **Description:** {4} 
-        **Link**: {5} 
+        *Title:* {0} by {1}
+        *Rating:* {2} by {3} users
+        *Description:* {4}
+        *Link*: {5}
 
         Tip: {6}""")
     title = book_data['title']
