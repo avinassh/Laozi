2eee027a190d5d332d84f4121285847ffaf5b827
avi <hi@avi.im>
2017-04-23 14:51:43 +0530

    Add group restrictions

--- lambda.py
+++ lambda.py
@@ -4,19 +4,41 @@ import telegram
 
 from goodreads_api import get_book_details_by_name, BookNotFound
 from utils import get_formatted_book_data
-from settings import TELEGRAM_ACCESS_TOKEN
+from settings import (TELEGRAM_ACCESS_TOKEN, ALLOWED_CHAT_TYPES,
+                      ALLOWED_CHAT_IDS)
 
 
 def lambda_handler(event, context):
     bot = telegram.Bot(token=TELEGRAM_ACCESS_TOKEN)
     data = json.loads(event['body'])
-
     try:
         message_id = data['message']['message_id']
         chat_id = data['message']['chat']['id']
+        chat_type = data['message']['chat']['type']
         text = data['message']['text']
         print("Received message_id: {}, chat_id: {}, text: {}".format(
             message_id, chat_id, text))
+
+        if chat_type not in ALLOWED_CHAT_TYPES:
+            response = ("Laozi is not enabled for this chat type. Contact "
+                        "the bot creator to enable.")
+            response = response + '\n\n' + json.dumps(data)
+            bot.sendMessage(reply_to_message_id=message_id,
+                            chat_id=chat_id, text=response,
+                            disable_web_page_preview=True,
+                            parse_mode='Markdown')
+            return
+
+        if chat_id not in ALLOWED_CHAT_IDS:
+            response = ("Laozi is now a paid bot and is not enabled for this "
+                        "group. To enable, contact the bot creator.")
+            response = response + '\n\n' + json.dumps(data)
+            bot.sendMessage(reply_to_message_id=message_id,
+                            chat_id=chat_id, text=response,
+                            disable_web_page_preview=True,
+                            parse_mode='Markdown')
+            return
+
         response = parse_command(text=text)
         if response:
             bot.sendMessage(reply_to_message_id=message_id,
--- settings.py
+++ settings.py
@@ -7,3 +7,10 @@ GOOGLE_CUSTOM_SEARCH_CX = os.environ['GOOGLE_CUSTOM_SEARCH_CX']
 
 # Telegram Webhook settings
 WEBHOOK_URL = os.environ['WEBHOOK_URL']
+
+# Group restrictions
+# Chat is either private message or group chat
+# allowing Laozi only in groups but not in `private` or `channel`
+ALLOWED_CHAT_TYPES = ['group', 'supergroup']
+# Laozi will interact only with following Chat groups or private ids
+ALLOWED_CHAT_IDS = [-1001091893296, -1001013427720]
